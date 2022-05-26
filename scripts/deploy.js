3c0a181b80ec2bcc9ea7ecafff63a554232b6ff5
luuminhbigin <minh.luu@bigin.vn>
2022-05-26 13:17:05 +0700

    upload source

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Cactus = await hre.ethers.getContractFactory("Cactus");
+  const cactus = await Cactus.deploy();
+
+  await cactus.deployed();
+  console.log("CmCactusNaive deployed to:", cactus.address);
+
+  await cactus.mint("https://ipfs.io/ipfs/QmVb4cUioXVyEZyBd4uVkhusaE6ykuNhvMHAHMs6PC6ti7");
+  console.log("NFT successfuly minted");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
